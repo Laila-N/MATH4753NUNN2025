15e0e193858b1ffc75ce5851eb8ea0ec2553fc42
Laila-N <laila.a.nunn-1@ou.edu>
2025-10-17 21:32:38 -0500

    new function

---
+++ R/myncurve.R
@@ -0,0 +1,18 @@
+#' Normal Curve Function
+#'
+#' @param a a value of P(X<=a)
+#' @param mu mean
+#' @param sigma sd
+#'
+#' @returns plot of normal distribution w/ shaded area of P(X<=a) and displays the probability in the command line
+#' @export
+#'
+#' @examples myncurve(10,10,4)
+myncurve = function(a, mu, sigma){
+  curve(dnorm(x,mean=mu,sd=sigma), xlim = c(mu-3*sigma, mu+3*sigma))
+  xcurve <- seq(mu-3*sigma, a, length=2000)
+  ycurve <- dnorm(xcurve, mu, sigma)
+  polygon(c(mu-3*sigma, xcurve, a), c(0, ycurve, 0), col='Pink')
+  prob <- round(pnorm(a,mean=mu,sd=sigma), 4)
+  list(mu = mu, sigma = sigma, a=a, Probability=prob)
+}
