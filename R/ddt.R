735d718f54160bd26e67a607d35f7bf9501f474f
Laila-N <laila.a.nunn-1@ou.edu>
2025-10-24 21:00:14 -0500

    commit data

---
+++ R/ddt.R
@@ -0,0 +1,18 @@
+#' DDT Fish in Tennessee River
+#'
+#' A subset of data from the World Health Organization Global Tuberculosis
+#' Report ...
+#'
+#' @format ## `ddt`
+#' A data frame with 7,240 rows and 60 columns:
+#' \describe{
+#'   \item{RIVER}{River type}
+#'   \item{MILE}{number}
+#'   \item{SPECIES}{fish name}
+#'   \item{LENGTH}{lrngth in cm}
+#'   \item{WEIGHT}{number}
+#'   \item{DDT}{number}
+#'   ...
+#' }
+#' @source MS
+"ddt"
---
+++ R/fire.R
@@ -0,0 +1,13 @@
+#' FIREDAM Data
+#'
+#'
+#' @format ## `fire`
+#' data of a 15 x 2:
+#' \describe{
+#'   \item{DISTANCE}{distance from fire station in miles}
+#'   \item{DAMAGE}{x 1000 dolllars in damage}
+#'
+#'   ...
+#' }
+#' @source <>
+"fire"
