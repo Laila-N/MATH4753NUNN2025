5eb9ab0a6e55498ec7f215ed182a513391408eb2
Laila-N <laila.a.nunn-1@ou.edu>
2025-11-07 19:44:07 -0600

    new function

---
+++ R/myclt.R
@@ -0,0 +1,18 @@
+#' My Central Limit Theorem (CLT) Uniform Function
+#'
+#' @param n size of sample
+#' @param iter iterations
+#'
+#' @returns a histogram of the CLT uniform
+#' @importFrom stats runif
+#' @export
+#'
+#' @examples myclt(n=10, iter=10000)
+#'
+myclt=function(n,iter){
+  y=runif(n*iter,0,5) #A
+  data=matrix(y,nr=n,nc=iter,byrow=TRUE) #B
+  sm=apply(data,2,sum) #C
+  hist(sm)
+  sm
+}
